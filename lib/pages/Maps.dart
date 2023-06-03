e8af6a2438169f6bcb664221e6df3f0c39a0d1c2
Daniele Poli <daniele.poli@edu.itspiemonte.it>
2023-06-03 13:25:11 +0200

    changed the back button of the details and added the details of the maps

--- lib/pages/Maps.dart
+++ lib/pages/Maps.dart
@@ -37,7 +37,7 @@ class _MapsPageState extends State<MapsPage> {
       backgroundColor: const Color.fromARGB(255, 30, 30, 30),
       appBar: AppBar(
         toolbarHeight: 100,
-        leading: Icon(Icons.arrow_back),
+        leading: const Icon(Icons.arrow_back_ios_new),
         title: Center(
           child: Container(
             margin: const EdgeInsets.only(top: 30, right: 60),
@@ -73,9 +73,9 @@ class _MapsPageState extends State<MapsPage> {
                                   _maps[index]["listViewIcon"],
                                 ))),
                         child: Text(
-                          _maps[index]["displayName"],
+                          _maps[index]["displayName"].toUpperCase(),
                           style: const TextStyle(
-                            fontFamily: 'valorant',
+                            fontFamily: 'monument',
                             fontSize: 26,
                             color: Colors.white,
                           ),
@@ -103,30 +103,127 @@ class Details extends StatelessWidget {
   Widget build(BuildContext context) {
     // Usa l'oggetto `maps` per visualizzare i dettagli delle mappe selezionate
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: const Color.fromARGB(255, 30, 30, 30),
       appBar: AppBar(
-        title: const Text('Skin Details'),
-        backgroundColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 30, 30, 30),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        toolbarHeight: 100,
+        title: Center(
+          child: Container(
+            margin: const EdgeInsets.only(top: 0, right: 60),
+            child: Text(
+              maps["displayName"].toUpperCase(),
+              style: const TextStyle(
+                fontFamily: 'monument',
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-              child: Image.network(
-                maps["splash"],
-                width: MediaQuery.of(context).size.width,
-                height: 500,
-                fit: BoxFit.cover,
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0),
+              ), // Image border
+              child: SizedBox.fromSize(
+                size: const Size.fromRadius(200), // Image radius
+                child: Image.network(
+                  maps["splash"],
+                  width: MediaQuery.of(context).size.width,
+                  height: 420,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            const SizedBox(height: 30),
-            Text(
-              maps["displayName"],
-              style: const TextStyle(
-                fontFamily: 'valorant',
-                fontSize: 26,
-                color: Colors.white,
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 30),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "COORDINATES",
+                      style: TextStyle(
+                        fontFamily: 'monument',
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      maps["coordinates"],
+                      style: const TextStyle(
+                        fontFamily: 'poppins',
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "DESCRIPTION",
+                      style: TextStyle(
+                        fontFamily: 'monument',
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    "A mysterious structure housing an astral conduit radiates with ancient power. Great stone doors provide a variety of movement opportunities and unlock the paths to three mysterious sites.",
+                    style: TextStyle(
+                      fontFamily: 'poppins',
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "MAP",
+                      style: TextStyle(
+                        fontFamily: 'monument',
+                        fontSize: 24,
+                        color: Colors.white,
+                      ), // will align at leading-end of parent/containe
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.rotationZ(
+                      6.27 / 4,
+                    ),
+                    child: Image.network(
+                      maps["displayIcon"],
+                      width: 340,
+                      height: 340,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 70),
+                ],
               ),
             ),
           ],
