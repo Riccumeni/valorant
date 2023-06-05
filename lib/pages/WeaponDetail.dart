fe5b655d63197ecc8707851c04a1879e55294169
Riccumeni <77925342+Riccumeni@users.noreply.github.com>
2023-06-05 15:10:20 +0200

    Create WeaponDetail.dart

---
+++ lib/pages/WeaponDetail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WeaponDetail extends StatelessWidget {
+  const WeaponDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
