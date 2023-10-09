0f49babaa7d7e8e620777d2ed34e0f04c849ae64
Riccumeni <77925342+Riccumeni@users.noreply.github.com>
2023-10-09 17:09:08 +0200

    adding filter by dropdown and textfield

--- lib/business_logic/bloc/champion/champions_cubit.dart
+++ lib/business_logic/bloc/champion/champions_cubit.dart
@@ -9,12 +9,34 @@ class ChampionsCubit extends Cubit<ChampionsState> {
 
   ValorantRepository repository = ValorantRepository();
 
-  Future<void> getChampions() async {
+  Future<void> getChampions({text, category}) async {
 
     emit(LoadingChampionsState());
 
+    if(category == "all"){
+      category = null;
+    }
+
     try{
       var championsResponse = await repository.getChampions();
+
+      if(text != null || category != null){
+        var newChampionResponse = [];
+        championsResponse.data!.forEach((champion){
+          if(champion.role?.displayName.toLowerCase() == category.toString().toLowerCase()){
+            if(text != null){
+              RegExp pattern = RegExp("^$text");
+              if(pattern.hasMatch(champion.displayName?.toLowerCase() ?? "")){
+                newChampionResponse.add(champion);
+              }
+            }else{
+              newChampionResponse.add(champion);
+            }
+          }
+        });
+        championsResponse.data = newChampionResponse.cast<Champion>();
+      }
+
       emit(SuccessChampionsState(championsResponse: championsResponse));
     }catch(e){
       emit(ErrorChampionsState());
--- lib/ui/pages/ChampionList.dart
+++ lib/ui/pages/ChampionList.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
@@ -15,11 +13,34 @@ class ChampionList extends StatefulWidget {
 }
 
 class _ChampionListState extends State<ChampionList> {
+
+  TextEditingController _controller = TextEditingController();
+
+  final category = <String>[];
+
+  var selected = "";
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     BlocProvider.of<ChampionsCubit>(context).getChampions();
+
+    category.add("all");
+    category.add("initiator");
+    category.add("duelist");
+    category.add("sentinel");
+    category.add("controller");
+
+    setState(() {
+      selected = category[0];
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -40,86 +61,139 @@ class _ChampionListState extends State<ChampionList> {
             ),
           ),
         ),
-        body: BlocBuilder<ChampionsCubit, ChampionsState>(
-          builder: (context, state) {
-            if (state is SuccessChampionsState) {
-              return GridView.builder(
-                itemCount: state.championsResponse!.data!.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisExtent: 230,
-                    mainAxisSpacing: 40),
-                itemBuilder: (_, index) {
-                  return InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ChampionDetail(
-                              champ: state.championsResponse!.data![index]),
-                        ),
-                      );
-                    },
-                    child: Stack(
-                      alignment: Alignment.topCenter,
-                      fit: StackFit.passthrough,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/champ-container.svg',
-                          width: 160,
-                          height: 130,
-                          color: Color(int.parse(
-                              "0xFF${state.championsResponse!.data![index]!.backgroundGradientColors![1].toString().substring(0, 6)}")),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                                child: CachedNetworkImage(
-                              imageUrl: state.championsResponse!.data![index]!.bustPortrait ?? "",
-                            )),
-                            Container(
-                                margin: EdgeInsets.only(bottom: 20),
-                                child: Text(
-                                  "${state.championsResponse!.data![index]!.displayName}"
-                                      .toUpperCase(),
-                                  style: const TextStyle(
-                                      color: Color.fromARGB(255, 30, 30, 30),
-                                      fontFamily: 'monument',
-                                      fontSize: 22),
-                                )
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                },
-              );
-            }
-            else if(state is LoadingChampionsState){
-              return const Center(
-                child: CircularProgressIndicator(color: Color.fromARGB(255,235, 86, 91),),
-              );
-            }
-            else{
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
-                  const Icon(Icons.dangerous_outlined, color: Color.fromARGB(255,235, 86, 91), size: 60,),
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
                   Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const Text("Something was wrong, check your internet connection",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'monument',
-                          fontSize: 14),
-                      textAlign: TextAlign.center,
+                    margin: const EdgeInsets.only(top: 20, bottom: 20),
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(
+                        controller: _controller,
+                        onSubmitted: (String value){
+                          BlocProvider.of<ChampionsCubit>(context).getChampions(text: value, category: selected);
+                        },
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),),
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),),
+                          hintStyle: TextStyle(color: Colors.white70),
+                          hintText: 'Type here...',
+                        ),
+                      ),
                     ),
+                  ),
+                  DropdownButton<String>(
+                    style: const TextStyle(color: Colors.white),
+                    dropdownColor: Colors.black,
+                    value: selected,
+                      items: category.map<DropdownMenuItem<String>>((String e) {
+                        return DropdownMenuItem<String>(
+                        value: e,
+                        child: Text(e),
+                        );
+                      }).toList(),
+                      onChanged: (value){
+                        setState(() {
+                          selected = value!;
+                          BlocProvider.of<ChampionsCubit>(context).getChampions(category: value);
+                        });
+                      }
                   )
                 ],
-              );
-            }
-          },
+              ),
+            ),
+            Expanded(
+              child: BlocBuilder<ChampionsCubit, ChampionsState>(
+                builder: (context, state) {
+                  if (state is SuccessChampionsState) {
+                    return GridView.builder(
+                      itemCount: state.championsResponse!.data!.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisExtent: 230,
+                          mainAxisSpacing: 40),
+                      itemBuilder: (_, index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => ChampionDetail(
+                                    champ: state.championsResponse!.data![index]),
+                              ),
+                            );
+                          },
+                          child: Stack(
+                            alignment: Alignment.topCenter,
+                            fit: StackFit.passthrough,
+                            children: [
+                              SvgPicture.asset(
+                                'assets/champ-container.svg',
+                                width: 160,
+                                height: 130,
+                                color: Color(int.parse(
+                                    "0xFF${state.championsResponse!.data![index]!.backgroundGradientColors![1].toString().substring(0, 6)}")),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                      child: CachedNetworkImage(
+                                    imageUrl: state.championsResponse!.data![index]!.bustPortrait ?? "",
+                                  )),
+                                  Container(
+                                      margin: EdgeInsets.only(bottom: 20),
+                                      child: Text(
+                                        "${state.championsResponse!.data![index]!.displayName}"
+                                            .toUpperCase(),
+                                        style: const TextStyle(
+                                            color: Color.fromARGB(255, 30, 30, 30),
+                                            fontFamily: 'monument',
+                                            fontSize: 22),
+                                      )
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }
+                  else if(state is LoadingChampionsState){
+                    return const Center(
+                      child: CircularProgressIndicator(color: Color.fromARGB(255,235, 86, 91),),
+                    );
+                  }
+                  else{
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:  [
+                        const Icon(Icons.dangerous_outlined, color: Color.fromARGB(255,235, 86, 91), size: 60,),
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          child: const Text("Something was wrong, check your internet connection",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'monument',
+                                fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ));
   }
+
+  /*
+  */
 }
